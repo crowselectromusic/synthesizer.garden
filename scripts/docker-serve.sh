0a1473d1cbc893d65e32e2296841fd69ac692a64
Cory Alder <coryalder@gmail.com>
2026-03-05 20:43:02 -0800

    migrate data from single data.json to <slug>.json

--- scripts/docker-serve.sh
+++ scripts/docker-serve.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$(realpath "$0")")/../" || exit 255
 
-docker run -u "$(id -u):$(id -g)" -v "$(pwd)":/app --workdir /app -p 8080:8080 ghcr.io/getzola/zola:v0.17.1 serve --interface 127.0.0.1 --port 9070 --base-url localhost
+docker run -u "$(id -u):$(id -g)" -v "$(pwd)":/app --workdir /app -p 8080:8080 ghcr.io/getzola/zola:v0.17.1 serve --interface 0.0.0.0 --port 8080 --base-url localhost
